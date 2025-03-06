1b2a75ec736fd1cba2b664d3ea936a9ab149ad5e
Saacfl6 <isaacreyflo@gmail.com>
2025-03-06 10:34:25 -0600

    Ejercicio 1

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -4,9 +4,9 @@
 int main() {
     int n;
     printf("Ingrese el tamaño del arreglo: ");
-    
-
+    scanf("%d", &n);
     int *arr;
+    arr = (int*)malloc(n *sizeof(int));
     if (arr == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
         return 1;
@@ -14,13 +14,16 @@ int main() {
 
     printf("Ingrese %d valores:\n", n);
     for (int i = 0; i < n; i++) {
-        
+        printf("Valor %d: ", i+1);
+        scanf("%d", (arr+i));
     }
 
     printf("El arreglo es: ");
+    printf("[\t");
     for (int i = 0; i < n; i++) {
-        
+        printf("%d\t", *(arr+i));
     }
+    printf("]");
     printf("\n");
 
     
